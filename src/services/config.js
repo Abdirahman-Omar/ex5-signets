38ceb93ef22d7f576c381d56ec03a348a4298a9a
Abdirahman Omar <abdirahmano217@gmail.com>
2021-03-24 22:07:54 -0400

    config

--- src/services/config.js
+++ src/services/config.js
@@ -5,7 +5,13 @@
 
 // Configuration Firebase (mettez-y les vôtres !)
 const firebaseConfig = {
-  
+  apiKey: "AIzaSyBqQRS8GaaRalAqeY3_-BLptJKi6GPMeig",
+  authDomain: "abdirahman-react.firebaseapp.com",
+  projectId: "abdirahman-react",
+  storageBucket: "abdirahman-react.appspot.com",
+  messagingSenderId: "302817357703",
+  appId: "1:302817357703:web:65e9f0f6459afea9d36d22",
+  measurementId: "G-3XYZ2HK4NZ"
 };
 export default firebaseConfig;
 
